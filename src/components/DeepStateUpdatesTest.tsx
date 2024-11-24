fc8aef8d5a1c9e798d05d4c6f8c10e44d965e94e
zoicho <daveyrow@gmail.com>
2024-11-24 17:47:40 +0100

    update

--- src/components/DeepStateUpdatesTest.tsx
+++ src/components/DeepStateUpdatesTest.tsx
@@ -42,7 +42,7 @@ export default function DeepStateUpdatesTest() {
 
     const interval = setInterval(() => {
       updateItems()
-    }, 10)
+    }, 200)
 
     return () => {
       clearInterval(interval)
